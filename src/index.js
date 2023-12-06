6496cc3544e74af98f80157f13b12f9234e7de1a
Denis <rainbowuran@gmail.com>
2023-12-06 14:11:04 +0300

    add html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('it work!!!')
+import  './styles.scss';
+import  'bootstrap';
+import * as yup from 'yup'
+
+import "~bootstrap/scss/bootstrap";
+
+export const schema = yup.object({
+    url: yup.string().url().required()
+})
+
+export function test(){
+document.getElementsByClassName('rss-form').addEventListener('submit',(e)=>{
+    e.preventDefalut()
+    
+    alert('ok')
+})
+}
+
